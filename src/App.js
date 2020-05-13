c012aa7c9c249d993bf1d38f419f805b5ddf6317
Muhammad Hamza <megahertz1999@gmail.com>
2020-05-14 01:39:39 +0500

    added routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Login from './pages/Login';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Register from './pages/Register';
+import ProductDetail from './pages/ProductDetail';
+import Shop from './pages/Shop';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
+          <Route exact path='/detail' component={ProductDetail} />
+          <Route exact path='/cart' component={ShoppingCart} />
+          <Route exact path='/shop' component={Shop} />
         </Switch>
       </BrowserRouter>
     </div>
