9f3a306f0e1f29a0ff1108cf6c7faf7a749e5492
Muhammad Hamza <megahertz1999@gmail.com>
2020-05-14 01:31:22 +0500

    added shop main pae component

---
+++ src/pages/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShopBanner from '../components/Shop/ShopBanner';
+import ProductsShop from '../components/Shop/ProductsShop';
+
+export default function Shop() {
+  return (
+    <>
+      <ShopBanner />
+      <ProductsShop />
+    </>
+  );
+}
